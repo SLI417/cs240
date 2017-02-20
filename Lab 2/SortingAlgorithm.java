3e26238bc03d6a455325c4c47114d50f0c857b71
Owl9 <theonlypersonhere@gmail.com>
2017-02-19 20:15:01 -0800

    Update SortingAlgorithm.java

--- Lab 2/SortingAlgorithm.java
+++ Lab 2/SortingAlgorithm.java
@@ -1,22 +1,7 @@
 package test;
-/**
- * This file pedagogical material for the course
- * CS 240: Data Structures and Algorithms
- * taught at California State Polytechnic University - Pomona, and
- * cannot be used without express written consent from the author.
- * 
- * Copyright (c) 2012 - Edwin Rodr&iacute;guez.
- */
-
-
 import java.util.Arrays;
 import java.util.Random;
 
-
-/**
- * @author Edwin Rodr&iacute;guez
- * 
- */
 public class SortingAlgorithm {
 
 	/**
